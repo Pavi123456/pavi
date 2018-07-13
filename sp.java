8bd269ac9f9fa7aae398a2e7b0d85c66bc4d2604
pavisindhu <41136803+pavisindhu@users.noreply.github.com>
2018-07-13 17:48:26 +0530

    Create sp.java

---
+++ sp.java
@@ -0,0 +1,17 @@
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		char ch='0';
+		if(ch>='a' && ch<='z'){
+			System.out.println("Alphabet");
+			
+		}
+
+
+	else{
+		System.out.println("No");
+	}
+}
+}
