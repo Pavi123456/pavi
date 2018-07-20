78ae330dbbe1a6312c59a61ac49da827309e85be
pavisindhu <41136803+pavisindhu@users.noreply.github.com>
2018-07-20 12:27:11 +0530

    Create Hello.java

---
+++ Hello.java
@@ -0,0 +1,12 @@
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		int b=2;
+		String a="Hello";
+		for(b=1;b<=2;b++){
+			System.out.println(a);
+		}
+	}
+}
