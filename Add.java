e3e37fe1d954e19c3766e6381194020f6ea1ef21
pavisindhu <41136803+pavisindhu@users.noreply.github.com>
2018-07-20 14:23:59 +0530

    Create Add.java

---
+++ Add.java
@@ -0,0 +1,20 @@
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		int [] arr=new int[20];
+		
+		int n=5;
+		arr[1]=1;
+		arr[2]=2;
+		arr[3]=3;
+		arr[4]=4;
+		arr[5]=5;
+		int b=2,i,sum=0;
+	            for(i=1;i<=b;i++){
+			sum=sum+arr[i];
+		}
+		System.out.println(sum);
+	}
+}
