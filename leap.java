35cede82e48972c4e61fdde17ad205ab70fcd6f9
pavisindhu <41136803+pavisindhu@users.noreply.github.com>
2018-07-20 12:20:40 +0530

    Create leap.java

---
+++ leap.java
@@ -0,0 +1,14 @@
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		int a=2000;
+		if(a%4==0){
+			System.out.println("yes");
+		}
+		else{
+			System.out.println("no");
+		}
+	}
+}
