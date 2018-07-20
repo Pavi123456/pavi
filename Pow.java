9f81d72dc0cc0bd323c9e9830e07de39a370cded
pavisindhu <41136803+pavisindhu@users.noreply.github.com>
2018-07-20 14:51:22 +0530

    Create Pow.java

---
+++ Pow.java
@@ -0,0 +1,11 @@
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		int a=2;
+		int b=3;
+		int c=(int)Math.pow(a,b);
+		System.out.println(c);
+	}
+}
