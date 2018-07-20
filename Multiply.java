b50f69c01934f12d1d5a0ea35434261afc2071e2
pavisindhu <41136803+pavisindhu@users.noreply.github.com>
2018-07-20 13:52:00 +0530

    Create Multiply.java

---
+++ Multiply.java
@@ -0,0 +1,13 @@
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		int a=3;
+		int b=1;
+		for(int i=1;i<=a;i++){
+			b=b*i;
+		}
+		System.out.println(b);
+	}
+}
